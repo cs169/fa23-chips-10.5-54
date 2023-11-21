c4811f77eda686856d94cfdcb264e24e7cfdf822
Justine Yuan <justineyuan@berkeley.edu>
2023-11-21 21:45:59 +0000

    [refactor] representative_spec.rb

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -7,11 +7,10 @@ describe Representative, type: :model do
     before do
       @rep_info = instance_double(Google::Apis::CivicinfoV2::RepresentativeInfoResponse)
       @existing_representative = described_class.create(name: 'name', ocdid: 0, title: 'title')
-      @new_representative = OpenStruct.new(name: 'new name', ocdid: 1, title: 'new title')
       allow(described_class).to receive(:create!).and_call_original
     end
 
-    context 'when representative already in database' do
+    context 'when representative is already in database' do
       before do
         allow(@rep_info).to receive(:officials).and_return([OpenStruct.new(name: @existing_representative.name)])
         offices_object = OpenStruct.new(name: @existing_representative.title,
@@ -26,8 +25,9 @@ describe Representative, type: :model do
       end
     end
 
-    context 'when representative not in database' do
+    context 'when searching for a new representative' do
       before do
+        @new_representative = OpenStruct.new(name: 'new name', ocdid: 1, title: 'new title')
         allow(@rep_info).to receive(:officials).and_return([OpenStruct.new(name: @new_representative.name)])
         new_offices_object = OpenStruct.new(name: @new_representative.title,
                                             division_id: @new_representative.ocdid, official_indices: [0])
