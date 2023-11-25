7a45dc72ee9930243e3642c30e6d875526a8e74b
Kevin Jiang <jhw021118@berkeley.edu>
2023-11-25 07:48:22 +0000

    iter1 part2

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -2,26 +2,26 @@
 
 class Representative < ApplicationRecord
   has_many :news_items, dependent: :delete_all
-
   def self.civic_api_to_representative_params(rep_info)
     reps = []
-
     rep_info.officials.each_with_index do |official, index|
       ocdid_temp = ''
       title_temp = ''
-
       rep_info.offices.each do |office|
         if office.official_indices.include? index
           title_temp = office.name
           ocdid_temp = office.division_id
         end
       end
-
-      rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-          title: title_temp })
+      rep_attributes = { name: official.name, ocdid: ocdid_temp, title: title_temp,
+        party: official.party, photo_url: official.photo_url }
+      if official.address
+        rep_attributes.merge!({ street: official.address[0].line1, city: official.address[0].city,
+                                state: official.address[0].state, zip:  official.address[0].zip })
+      end
+      rep = Representative.find_or_create_by!(rep_attributes)
       reps.push(rep)
     end
-
     reps
   end
 end
