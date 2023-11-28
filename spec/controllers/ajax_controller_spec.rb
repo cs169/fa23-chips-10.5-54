4c2fc0db766e2bd875a2ad27c4cbf9940926cd62
Yihan Chen <yhchen2016@berkeley.edu>
2023-11-28 07:22:49 +0000

    some tests still failed

--- spec/controllers/ajax_controller_spec.rb
+++ spec/controllers/ajax_controller_spec.rb
@@ -1,9 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe AjaxController, type: :controller do
-  let!(:state) { State.create(symbol: 'CA', name: 'California', fips_code: '06') }
+  let!(:state) { 
+    State.create(
+      symbol: 'CA', 
+      name: 'California', 
+      fips_code: '06', 
+      is_territory: false,
+      lat_min: 32.5, 
+      lat_max: 100,
+      long_min: 500,
+      long_max: 2000,
+      created_at: Time.now, 
+      updated_at: Time.now
+    )
+  }
   let!(:counties) { 
-    3.times.map { |i| County.create(name: "County #{i}", state: state) }
+    3.times.map { |i| 
+      County.create(
+        name: "County #{i}", 
+        state: state,
+        fips_code: '06', 
+        # lat_min: 32.5, 
+        # lat_max: 100,
+        # long_min: 500,
+        # long_max: 2000,
+        # created_at: Time.now, 
+        # updated_at: Time.now
+      ) 
+    }
   }
 
   describe 'GET #counties' do
@@ -11,7 +36,8 @@ RSpec.describe AjaxController, type: :controller do
       it 'returns the counties for the given state as JSON' do
         get :counties, params: { state_symbol: state.symbol }, format: :json
         expect(response.content_type).to eq('application/json; charset=utf-8')
-        expect(JSON.parse(response.body)).to eq(counties.map { |county| { 'id' => county.id, 'name' => county.name } })
+        expected_response = counties.map { |county| { 'id' => county.id, 'name' => county.name } }
+        expect(JSON.parse(response.body)).to match_array(expected_response)
       end
     end
 
@@ -24,4 +50,3 @@ RSpec.describe AjaxController, type: :controller do
     end
   end
 end
-
