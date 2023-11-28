ce8a8b088af372ebce26f96a8287c6bbadfe7dac
Yihan Chen <yhchen2016@berkeley.edu>
2023-11-28 07:55:06 +0000

    fix ajax tst

--- spec/controllers/ajax_controller_spec.rb
+++ spec/controllers/ajax_controller_spec.rb
@@ -21,6 +21,7 @@ RSpec.describe AjaxController, type: :controller do
         name: "County #{i}", 
         state: state,
         fips_code: '06', 
+        fips_class: '1'
         # lat_min: 32.5, 
         # lat_max: 100,
         # long_min: 500,
@@ -35,7 +36,7 @@ RSpec.describe AjaxController, type: :controller do
     context 'when state is found' do
       it 'returns the counties for the given state as JSON' do
         get :counties, params: { state_symbol: state.symbol }, format: :json
-        expect(response.content_type).to eq('application/json; charset=utf-8')
+        expect(response.content_type).to eq('application/json')
         expected_response = counties.map { |county| { 'id' => county.id, 'name' => county.name } }
         expect(JSON.parse(response.body)).to match_array(expected_response)
       end
